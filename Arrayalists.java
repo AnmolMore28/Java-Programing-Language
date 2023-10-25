3f0261dbf5a78718426612445f5c092f9cbe0a6d
Anmol More <anmolmore1256@gmail.com>
2023-10-25 15:59:53 +0530

    added swaping in Arraylist

--- Arrayalists.java
+++ Arrayalists.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
 public class Arrayalists {
-//   public static void swap(ArrayList<Integer>list, int idx1,int idx2){
-// int temp = list.get(idx1);
+  public static void swap(ArrayList<Integer>list, int idx1,int idx2){
+int temp = list.get(idx1);
+list.set(idx1, list.get(idx2));
+list.set(idx2, temp);
+
+}
+
+
+
 
   
   public static void main(String args[] ){
 
         
-  // ArrayList<Integer> list= new ArrayList<>();
+  //  ArrayList<Integer> list= new ArrayList<>();
 //  ArrayList<String> list2 = new ArrayList<>();
 //  ArrayList<Boolean> list3 = new ArrayList<>();
   
-//  list.add(2); //here complexity is O(1)
-//  list.add(5);
-//  list.add(9);
-//  list.add(6);
-//  list.add(8);
+  // list.add(2); //here complexity is O(1)
+  // list.add(5);
+  // list.add(9);
+  // list.add(6);
+  // list.add(8);
 // // reverse Descending
 // for (int i = list.size()-1; i >=0; i--) {
 // System.out.print(list.get(i)+" ");
@@ -82,30 +89,44 @@ list2 = 2,4,5,6,8,10
 list3 = 3,6,9,12,15*/ 
 
 // establish the main list
- ArrayList<ArrayList<Integer>>mainlist =new ArrayList<>();
-//  cerating l1
-ArrayList<Integer> list1 = new ArrayList<>();
-ArrayList<Integer> list2 = new ArrayList<>();
-ArrayList<Integer> list3 = new ArrayList<>();
- for (int i = 1; i <= 5; i++) {
-  list1.add(i*1);
-  list2.add(i*2);
-  list3.add(i*3);
+//  ArrayList<ArrayList<Integer>>mainlist =new ArrayList<>();
+// //  cerating l1
+// ArrayList<Integer> list1 = new ArrayList<>();
+// ArrayList<Integer> list2 = new ArrayList<>();
+// ArrayList<Integer> list3 = new ArrayList<>();
+//  for (int i = 1; i <= 5; i++) {
+//   list1.add(i*1);
+//   list2.add(i*2);
+//   list3.add(i*3);
 
-  }
-// Add it to mainlist
-mainlist.add(list1);
-mainlist.add(list2);
-mainlist.add( list3);
-System.out.println(mainlist);
+//   }
+// // Add it to mainlist
+// mainlist.add(list1);
+// mainlist.add(list2);
+// mainlist.add( list3);
+// System.out.println(mainlist);
 
-//printing loop
-for (int i = 0; i < mainlist.size(); i++) {
-   ArrayList<Integer> currentlist= mainlist.get(i);
-   for (int j = 0; j < currentlist.size(); j++) {
-     System.out.print(currentlist.get(j)+" ");
-   }System.out.println();
- }System.out.println(mainlist);
-  
-} 
+// //printing loop
+// for (int i = 0; i < mainlist.size(); i++) {
+//    ArrayList<Integer> currentlist= mainlist.get(i);
+//    for (int j = 0; j < currentlist.size(); j++) {
+//      System.out.print(currentlist.get(j)+" ");
+//    }System.out.println();
+//  }System.out.println(mainlist);
+   
+
+
+// For Swapping
+
+ArrayList<Integer> list= new ArrayList<>();
+list.add(2); //here complexity is O(1)
+  list.add(5);
+  list.add(9);
+  list.add(3);
+  list.add(6);
+
+ int idx1 =1,idx2=3;
+ System.out.println(list);
+swap(list, idx1, idx2);
+System.out.println(list); } 
 }
