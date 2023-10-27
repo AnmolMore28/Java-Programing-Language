08b88bc016b823bb79a05e45f99f56e28eac6929
Anmol More <anmolmore1256@gmail.com>
2023-10-27 14:13:22 +0530

    .

--- OOPS.java
+++ OOPS.java
@@ -8,25 +8,44 @@ public class OOPS{
     // p1.setColor("Yellow");
     p1.color = "yellow";
    System.out.println(p1.color);
-    BankAccount myAcc =new BankAccount(); 
-  myAcc.username="Anmol";
-  myAcc.setPassword("AnmolkRam");;
+
+ Student s1= new Student();
+
+
+
+   
+   
+   //     BankAccount myAcc =new BankAccount(); 
+//   myAcc.username="Anmol";
+//   myAcc.setPassword("AnmolkRam");
 }
     }
- class BankAccount{
-  public String username;
-         private String password;
-   public void setPassword (String pwd){
-    password=pwd;
-   }
-        }  
+//  class BankAccount{
+//   public String username;
+//          private String password;
+//    public void setPassword (String pwd){
+//     password=pwd;
+//    }
+//         }  
+
+class Student {
+    String name;
+    int roll;
+    String password;
+Student (Student s1){
+    System.out.println(" Ram Ram");
+
+}
+Student(){
+System.out.println("Ram bhai Sarayanne!!! ");
+}}
 class Pen{
     String color;
     int tip;
 void setColor(String newColor ){
     color =newColor;
 
-}
+} 
 void setTip(int newTip){
     tip =newTip;
 
