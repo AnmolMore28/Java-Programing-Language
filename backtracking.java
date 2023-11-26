76219de0da4ddf0411e12b2ef662d8ea7e446d59
Anmol More <anmolmore1256@gmail.com>
2023-11-26 14:59:00 +0530

    Added new Problem

--- backtracking.java
+++ backtracking.java
@@ -14,8 +14,22 @@ public static void findSubsets(String str,int i, String ans){
 // no choices
 findSubsets(str, i+1, ans);
 }
+
+public static void findPermutation(String str,String ans){
+if (str.length()==0) {
+  System.out.println(ans);
+  return; 
+}
+  for (int i = 0; i < str.length(); i++) {
+ char curr= str.charAt(i);
+ String Newstr =str.substring(0,i)+str.substring(i+1);
+ findPermutation(Newstr, ans+curr);
+}
+ }
+
  public static void main(String[] args) {
  String str = "abc";
-findSubsets(str, 0, "");
-}
+// findSubsets(str, 0, "");
+findPermutation(str, "" );
+ }
 }
