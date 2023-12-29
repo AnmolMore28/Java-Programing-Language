d8f1d8760290e3d180bf51ff152b1a7a4c090994
Anmol More <anmolmore1256@gmail.com>
2023-12-29 20:22:13 +0530

    created in systematic way

---
+++ LinkedListpt1/Linkedlist.java
@@ -0,0 +1,12 @@
+public class Linkedlist {
+    public static class Node{
+      int Node;
+     Node Data;
+     public class Node(int Node){
+
+     }
+    }
+    public static void main(String[] args) {
+    
+}
+}
